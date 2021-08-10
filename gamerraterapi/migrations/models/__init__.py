d1b8733839fee0360901c2316b554d69be11c0ec
Caleb Wagner <calebwagner5@gmail.com>
2021-08-10 07:41:17 -0500

    added classes in models to init file

---
+++ gamerraterapi/migrations/models/__init__.py
@@ -0,0 +1,7 @@
+
+from .category import Category
+from .game import Game
+from .game_category import GameCategory
+from .game_image import GameImage
+from .review import Review
+from .player import Player
--- gamerraterapi/migrations/models/game_category.py
+++ gamerraterapi/migrations/models/game_category.py
@@ -1,7 +1,7 @@
 
 from django.db import models
 
-class JoinCategory(models.Model):
+class GameCategory(models.Model):
     """JoinCategory Model: Join table between Category and Game
     """
     game = models.ForeignKey("Game", on_delete=models.CASCADE)
